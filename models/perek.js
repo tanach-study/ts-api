f328f788cd16dfb8666af8f3c08f98881ded5da5
Joey Pinhas <jpinhas97@yahoo.com>
2018-05-02 11:35:38 -0400

    Cleans up all the models

--- models/perek.js
+++ models/perek.js
@@ -1,25 +1,25 @@
 const { getDB } = require('../lib/dbConnection');
 
-function getOnePerek (req, res, next) {
+function getOnePerek(req, res, next) {
   const { sefer, perek } = req.params;
-  const queryPerek = String(parseInt(perek)) === 'NaN' ? perek : parseInt(perek);
-  getDB().then(db => {
+  const queryPerek = String(parseInt(perek, 10)) === 'NaN' ? perek : parseInt(perek, 10);
+  getDB().then((db) => {
     db.collection('perakim')
-    .findOne({
-      book_name: sefer,
-      perek_id: queryPerek,
-    }, {
-      _id: 0,
-    })
-    .then(data => {
-      res.data = data;
-      next();
-    })
-    .catch(findErr => next(findErr));
+      .findOne({
+        book_name: sefer,
+        perek_id: queryPerek,
+      }, {
+        _id: 0,
+      })
+      .then((data) => {
+        res.data = data;
+        next();
+      })
+      .catch(findErr => next(findErr));
   })
-  .catch(dbErr => next(dbErr));
+    .catch(dbErr => next(dbErr));
 }
 
 module.exports = {
   getOnePerek,
-}
+};
--- models/sefer.js
+++ models/sefer.js
@@ -1,32 +1,32 @@
 const { getDB } = require('../lib/dbConnection.js');
 
 function getAllSefarim(req, res, next) {
-  getDB().then(db => {
+  getDB().then((db) => {
     db.collection('books')
-    .find({}, { _id: 0 })
-    .sort({ 'seferMeta.book_id': 1 })
-    .toArray()
-    .then(sefarim => {
-      res.data = sefarim;
-      next();
-    })
-    .catch(findErr => next(findErr));
+      .find({}, { _id: 0 })
+      .sort({ 'seferMeta.book_id': 1 })
+      .toArray()
+      .then((sefarim) => {
+        res.data = sefarim;
+        next();
+      })
+      .catch(findErr => next(findErr));
   })
-  .catch(dbErr => next(dbErr));
+    .catch(dbErr => next(dbErr));
 }
 
 function getOneSefer(req, res, next) {
   const { sefer } = req.params;
-  getDB().then(db => {
+  getDB().then((db) => {
     db.collection('books')
-    .findOne({ 'seferMeta.book_name': sefer }, { _id: 0 })
-    .then(data => {
-      res.data = data;
-      next();
-    })
-    .catch(findErr => next(findErr));
+      .findOne({ 'seferMeta.book_name': sefer }, { _id: 0 })
+      .then((data) => {
+        res.data = data;
+        next();
+      })
+      .catch(findErr => next(findErr));
   })
-  .catch(dbErr => next(dbErr));
+    .catch(dbErr => next(dbErr));
 }
 
 module.exports = {
